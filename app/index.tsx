ff8559600658ff25ed03bc74f2e52d46576d72f2
liyunfu1998 <liyunfu1998@qq.com>
2024-10-06 17:34:24 +0800

    feat: 添加自定义颜色和预设class

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,9 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View className="btn">
+        <Text>Hello</Text>
+      </View>
     </View>
   );
 }
