73b66412e050f0c6db9b9d444baf705a8bdc03c1
liyunfu1998 <liyunfu1998@qq.com>
2024-11-08 15:22:30 +0800

    feat: 登录后跳转

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -34,7 +34,7 @@ export default function Page() {
 
       if (signInAttempt.status === "complete") {
         await setActive({ session: signInAttempt.createdSessionId });
-        router.replace("/");
+        router.replace("/(home)/(drawer)/(chat)/new");
       } else {
         console.error(JSON.stringify(signInAttempt, null, 2));
       }
