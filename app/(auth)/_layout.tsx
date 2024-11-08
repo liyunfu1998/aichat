19439716a3a5792aa69a757811decf6e47051f39
liyunfu1998 <liyunfu1998@qq.com>
2024-11-08 15:30:56 +0800

    chore: 格式化代码

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,18 +1,21 @@
 import { Redirect, Stack } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
 
-export default function AuthRoutesLayout(){
-  const {isSignedIn} = useAuth()
+export default function AuthRoutesLayout() {
+  const { isSignedIn } = useAuth();
 
-  if(isSignedIn){
-    return <Redirect href="/" />
+  if (isSignedIn) {
+    return <Redirect href="/" />;
   }
 
   return (
     <Stack>
-      <Stack.Screen name="sign-in" options={{
-        title: '登录'
-      }} />
+      <Stack.Screen
+        name="sign-in"
+        options={{
+          title: "登录",
+        }}
+      />
       <Stack.Screen
         name="sign-up"
         options={{
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,44 +1,45 @@
-import * as SecureStore from 'expo-secure-store'
+import * as SecureStore from "expo-secure-store";
 import { Slot } from "expo-router";
-import {ClerkProvider, ClerkLoaded} from '@clerk/clerk-expo'
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 
 const tokenCache = {
   async getToken(key: string) {
     try {
-      const item = await SecureStore.getItemAsync(key)
+      const item = await SecureStore.getItemAsync(key);
       if (item) {
-        console.log(`${key} was used 🔐 \n`)
+        console.log(`${key} was used 🔐 \n`);
       } else {
-        console.log('No values stored under key: ' + key)
+        console.log("No values stored under key: " + key);
       }
-      return item
+      return item;
     } catch (error) {
-      console.error('SecureStore get item error: ', error)
-      await SecureStore.deleteItemAsync(key)
-      return null
+      console.error("SecureStore get item error: ", error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
     }
   },
   async saveToken(key: string, value: string) {
     try {
-      return SecureStore.setItemAsync(key, value)
+      return SecureStore.setItemAsync(key, value);
     } catch (err) {
-      return
+      return;
     }
   },
   async deleteToken(key: string) {
     try {
-      return SecureStore.deleteItemAsync(key)
+      return SecureStore.deleteItemAsync(key);
     } catch (err) {
-      return
+      return;
     }
-  }
-}
+  },
+};
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
-
-if(!publishableKey){
-  throw new Error('Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env')
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
 }
 export default function RootLayout() {
   return (
