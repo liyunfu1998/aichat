5dc98c34d4fc3ac2dec1c4d515a569e061496fb1
liyunfu1998 <liyunfu1998@qq.com>
2024-11-14 10:44:11 +0800

    feat: 添加聊天界面样式

--- app/(home)/(drawer)/(chat)/new.tsx
+++ app/(home)/(drawer)/(chat)/new.tsx
@@ -1,10 +1,102 @@
 import { defaultStyles } from "@/constants/Styles";
 import { Stack } from "expo-router";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Image,
+  LayoutChangeEvent,
+} from "react-native";
 import HeaderDropDown from "@/components/HeaderDropDown";
 import { useState } from "react";
+import MessageInput from "@/components/MessageInput";
+import { useAuth } from "@clerk/clerk-expo";
+import MessageIdeas from "@/components/MessageIdeas";
+import { FlashList } from "@shopify/flash-list";
+import ChatMessage from "@/components/ChatMessage";
+import { Role } from "@/utils/Interfaces";
+
+const MockMessages = [
+  {
+    role: Role.Bot,
+    content: "Hello, how can I help you today?",
+  },
+  {
+    role: Role.User,
+    content:
+      "I want to learn about the stock market I want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock market",
+  },
+  {
+    role: Role.Bot,
+    content: "Hello, how can I help you today?",
+  },
+  {
+    role: Role.User,
+    content:
+      "I want to learn about the stock market I want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock market",
+  },
+  {
+    role: Role.Bot,
+    content: "Hello, how can I help you today?",
+  },
+  {
+    role: Role.User,
+    content:
+      "I want to learn about the stock market I want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock market",
+  },
+  {
+    role: Role.Bot,
+    content: "Hello, how can I help you today?",
+  },
+  {
+    role: Role.User,
+    content:
+      "I want to learn about the stock market I want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock market",
+  },
+  {
+    role: Role.Bot,
+    content: "Hello, how can I help you today?",
+  },
+  {
+    role: Role.User,
+    content:
+      "I want to learn about the stock market I want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock market",
+  },
+  {
+    role: Role.Bot,
+    content: "Hello, how can I help you today?",
+  },
+  {
+    role: Role.User,
+    content:
+      "I want to learn about the stock market I want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock market",
+  },
+  {
+    role: Role.Bot,
+    content: "Hello, how can I help you today?",
+  },
+  {
+    role: Role.User,
+    content:
+      "I want to learn about the stock market I want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock marketI want to learn about the stock market",
+  },
+];
 export default function New() {
+  const { signOut } = useAuth();
   const [gptVersion, setGptVersion] = useState("3.5");
+  const [messages, setMessages] = useState<any[]>(MockMessages);
+  const [height, setHeight] = useState(0);
+  const getCompletion = async (text: string) => {
+    setMessages([...messages, text]);
+  };
+
+  const onLayout = (event: LayoutChangeEvent) => {
+    const { height } = event.nativeEvent.layout;
+    setHeight(height / 2);
+  };
   return (
     <View style={defaultStyles.pageContainer}>
       <Stack.Screen
@@ -22,7 +114,61 @@ export default function New() {
           ),
         }}
       />
-      <Text>New</Text>
+      <View style={styles.page} onLayout={onLayout}>
+        {messages.length === 0 && (
+          <View style={[styles.logoContainer, { marginTop: height / 2 - 100 }]}>
+            <Image
+              source={require("@/assets/images/logo-white.png")}
+              style={styles.image}
+            />
+          </View>
+        )}
+        <FlashList
+          data={messages}
+          renderItem={({ item }) => <ChatMessage {...item} />}
+          estimatedItemSize={400}
+          contentContainerStyle={{
+            paddingBottom: 150,
+            paddingTop: 30,
+          }}
+          keyboardDismissMode="on-drag"
+        />
+      </View>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={70}
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          width: "100%",
+        }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        {messages?.length === 0 && (
+          <MessageIdeas onSelectCard={getCompletion} />
+        )}
+        <MessageInput onShouldSend={getCompletion} />
+      </KeyboardAvoidingView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logoContainer: {
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 50,
+    height: 50,
+    backgroundColor: "#000",
+    borderRadius: 50,
+  },
+  image: {
+    width: 30,
+    height: 30,
+    resizeMode: "cover",
+  },
+  page: {
+    flex: 1,
+  },
+});
